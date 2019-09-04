8aba09ae188d462371350245d1d2dc580da37bfa
Roddur Dasgupta <roddurd@gmail.com>
2019-09-03 19:07:03 -0700

    Location!

--- scraper.py
+++ scraper.py
@@ -15,16 +15,18 @@ client.close()
 
 html_soup = soup(html, "html.parser")
 
-menus = html_soup.findAll('ul',{'class':'menu-item-list'})
+menus = html_soup.findAll('div',{'class':'col-sm-6 col-md-4'})
 
-being_served = False
+being_served = (False,None)
+dining_hall = None
 
 for menu in menus:
+    dining_hall = str(menu.h3.text)
     menu = str(menu).lower()
     if fav_food in menu:
-        being_served = True
+        being_served = (True,dining_hall)
 
-result = "Yay! That food is being served today!" if being_served else "Sorry, that food is not being served today."
+result = "Yay! That food is being served at " + being_served[1] + " today!" if being_served[0] else "Sorry, that food is not being served today."
 
 print(result)
 
