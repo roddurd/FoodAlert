8d621dbc05ae2203ea5d6e4643634b49e0905e6c
Roddur Dasgupta <roddurd@gmail.com>
2019-09-03 19:16:01 -0700

    support for multiple locations; sets to avoid repetition

--- scraper.py
+++ scraper.py
@@ -1,10 +1,10 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
 
-fav_food = input("What food are you looking for? Use the exact name (case insensitive):")
+fav_food = input("What food are you looking for? Double check your spelling (case insensitive): ") 
 
 while not fav_food:
-    fav_food = input("What food are you looking for? Use the exact name (case insensitive):") 
+    fav_food = input("What food are you looking for? Double check your spelling (case insensitive): ") 
 fav_food = fav_food.lower()
 
 url = 'https://hospitality.usc.edu/residential-dining-menus/'
@@ -17,16 +17,17 @@ html_soup = soup(html, "html.parser")
 
 menus = html_soup.findAll('div',{'class':'col-sm-6 col-md-4'})
 
-being_served = (False,None)
-dining_hall = None
+being_served = []
 
 for menu in menus:
     dining_hall = str(menu.h3.text)
     menu = str(menu).lower()
     if fav_food in menu:
-        being_served = (True,dining_hall)
-
-result = "Yay! That food is being served at " + being_served[1] + " today!" if being_served[0] else "Sorry, that food is not being served today."
+        being_served.append(dining_hall)
+if being_served:
+    being_served = list(set(being_served))
+    location = " and ".join(being_served) if len(being_served) > 1 else being_served[0]
+result = "Yay! That food is being served at " + location + " today!" if being_served[0] else "Sorry, that food is not being served today."
 
 print(result)
 
